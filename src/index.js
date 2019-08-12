93dc51bee92316746ca9145f2f33852c2ec20336
hossam ali <dev.hossam.ali@gmail.com>
2019-08-13 00:19:25 +0200

    Quick Performance Test!

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import loadWasm from '@/wasm/src/lib.rs';
 
-console.log('I am alive!!!');
 
 loadWasm().then(result => {
   const { get_total } = result.instance.exports;
-  console.log("Number:: ", get_total(5000000));
+
+  console.time("wasm");
+  console.log(get_total(60000));
+  console.timeEnd("wasm");
+
+  console.time("js");
+  console.log(getTotal(60000));
+  console.timeEnd("js");
 });
+
+
+function getTotal(num) {
+  let total = 0;
+  for (let i = 0; i < num; i++) {
+    total += i;
+  }
+  return total;
+}
